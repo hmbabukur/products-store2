964c08a0acae30f395a24e6799db839531da78fd
hmbabukur <huzaifamuhammadbello@gmail.com>
2025-09-02 14:14:27 -0700

    Implemented product fetching with TanStack Query

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState, useContext } from 'react';
+import { useContext } from 'react';
 import { CartContext } from '../contexts/CartContext';
 import ProductCard from '../components/ProductsCard';
+import { useQuery } from "@tanstack/react-query";
 
 interface Product {
   id: number;
@@ -9,19 +10,32 @@ interface Product {
   thumbnail: string;
 }
 
+async function fetchProducts(): Promise<Product[]> {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const res = await fetch("https://dummyjson.com/products");
+  const data = await res.json();
+  return data.products;
+}
+
 function ProductList() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const { data: products, isLoading, isError, error } = useQuery<Product[]>({
+    queryKey: ["products"], 
+    queryFn: fetchProducts,
+  });
+
   const { addToCart } = useContext(CartContext)!;
 
-  useEffect(() => {
-    fetch('https://dummyjson.com/products')
-      .then((res) => res.json())
-      .then((data) => setProducts(data.products));
-  }, []);
+  if (isLoading){
+    return <p className='text-center text-xl mt-10'>Loading Products...</p>;
+  }
+
+  if (isError){
+    return <p className=' text-center text-xl mt-10 text-red-500'>Error message: {error.message}</p>
+  }
 
   return (
     <div className="grid lg:grid-cols-5 gap-8 py-6 px-14 md:px-10 w-full max-w-screen overflow-x-hidden">
-      {products.map((product: Product) => (
+      {products?.map((product: Product) => (
         <ProductCard key={product.id} product={product} onAddToCart={addToCart}/>
       ))}
     </div>
